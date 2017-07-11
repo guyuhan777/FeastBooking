82faf67dce760a1437855490fdc65e995e51a358
顾宇瀚 15365363655 <2534821384@qq.com>
2017-07-11 23:04:15 +0800

    Initial commit

---
+++ app/src/main/java/com/iplay/feastbooking/basic/BasicActivity.java
@@ -0,0 +1,51 @@
+package com.iplay.feastbooking.basic;
+
+import android.os.Build;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Window;
+import android.view.WindowManager;
+
+/**
+ * Created by admin on 2017/7/11.
+ */
+
+public abstract class BasicActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT){
+            setTranslucentStatus(true);
+        }
+        init();
+    }
+
+    public void init(){
+        setContentView();
+        findViews();
+        getData();
+        showContent();
+    }
+
+    private void setTranslucentStatus(boolean on){
+        Window window = getWindow();
+        WindowManager.LayoutParams winParams = window.getAttributes();
+        final int bits = WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS;
+        if(on){
+            winParams.flags |= bits;
+        }else{
+            winParams.flags &= ~bits;
+        }
+        window.setAttributes(winParams);
+    }
+
+    public abstract void setContentView();
+
+    public abstract void findViews();
+
+    public abstract void getData();
+
+    public abstract void showContent();
+}
---
+++ app/src/main/java/com/iplay/feastbooking/ui/HomePageActivity.java
@@ -0,0 +1,28 @@
+package com.iplay.feastbooking.ui;
+
+import com.iplay.feastbooking.R;
+import com.iplay.feastbooking.basic.BasicActivity;
+
+public class HomePageActivity extends BasicActivity {
+
+    @Override
+    public void setContentView() {
+        setContentView(R.layout.main_view);
+    }
+
+    @Override
+    public void findViews() {
+
+    }
+
+    @Override
+    public void getData() {
+
+    }
+
+    @Override
+    public void showContent() {
+
+    }
+
+}
